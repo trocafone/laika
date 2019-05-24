e949ea931a214350ef49af779641a0114f3cd674
Seva Gavrilov <gavrilovseva@gmail.com>
2019-05-24 10:01:16 -0300

    Update version to 1.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = u'laika'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1.2.0'
+release = u'1.3.0'
 
 
 # -- General configuration ---------------------------------------------------
